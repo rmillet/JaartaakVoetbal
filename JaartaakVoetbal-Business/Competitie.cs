35b7bf65f7b07d989ad0f4a713a09de7268a11cc
Ronny Millet <rmillet@imb-brugge.net>
2020-03-19 10:59:23 +0100

    Kadootje om te starten (R. Millet)

--- JaartaakVoetbal-Business/Competitie.cs
+++ JaartaakVoetbal-Business/Competitie.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace JaartaakVoetbal_Business
 {
-    public class Klassement
+    public class Competitie
     {
-
-        //hier komen je private fields
 
     }
 }
---
+++ JaartaakVoetbal-Business/Match.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaartaakVoetbal_Business
+{
+    public class Match
+    {
+
+        //hier komen je private fields
+        private string _thuisPloeg;
+        private string _bezoekersPloeg;
+        private int _thuisScore;
+        private int _bezoekersScore;
+        private DateTime _speeldag;
+
+
+        //methode
+        //override van ToString
+        public override string ToString()
+        {
+            return _thuisPloeg + " - " + _bezoekersPloeg + " (" + _thuisScore.ToString() + "-" + _bezoekersScore.ToString() + ")";
+        }
+
+    }
+}
